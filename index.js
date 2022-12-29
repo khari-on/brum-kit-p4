d6cb6a5cf03184c9c1f047a4ee00b469fa09c5eb
Hari Prasath <Khariprasath351@gmail.com>
2022-12-29 20:42:11 +0530

    added audio fileand eventlistner

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const drumButton = document.querySelectorAll('.drum')
+
+drumButton.forEach((container)=>{
+   container.addEventListener('click',(e)=>{
+       const buttonInnerHtml =e.target.innerHTML;
+       console.log(buttonInnerHtml)
+       audioPlay(buttonInnerHtml);
+   })  
+})
+
+
+function audioPlay(buttonInnerHtml){
+    
+    switch(buttonInnerHtml){
+        
+        case 'w':
+         audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+        case 'a':
+         audio = new Audio('./sounds/tom-2.mp3');
+        audio.play();
+         break;
+
+     case 's':
+        audio = new Audio('./sounds/tom-3.mp3');
+        audio.play();
+        break;
+
+     case 'd':
+        audio = new Audio('./sounds/tom-4.mp3');
+        audio.play();
+        break;
+    case 'j':
+            audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+     case 'k':
+         audio = new Audio('./sounds/kick-bass.mp3');
+         audio.play();
+        break;
+     case 'l':
+         audio = new Audio('./sounds/snare.mp3');
+         audio.play();
+         break;
+    }
+}
